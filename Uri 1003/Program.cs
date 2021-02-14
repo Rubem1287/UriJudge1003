1116ce8b263c2e6d373aae23b55b41afd88ad71d
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-14 18:19:58 -0300

    Add files via upload

---
+++ Uri 1003/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Uri_1003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, soma;
+
+            Console.WriteLine();
+            Console.Write("Digite o primeiro número : ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Digite o Segundo número : ");
+            b = int.Parse(Console.ReadLine());
+
+            soma = a + b;
+
+            Console.WriteLine();
+            Console.WriteLine("SOMA = "+soma);
+        }
+    }
+}
